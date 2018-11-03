841414b14c41b59b5cfaced478deddd1070ce62c
Andrew Ouatu <aouatu@outlook.com>
2018-11-02 17:56:33 -0700

    Initial project setup complete with dummy data

---
+++ src/dummy_data/list.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        title: 'Go to store',
+        details: 'Get eggs, bread, milk, and diet coke',
+        _id: '001'
+    },
+    {
+        title: 'Go to gas station',
+        details: 'Get gas',
+        _id: '002'
+    },
+    {
+        title: 'Go to Home Depot',
+        details: 'Buy screwdriver',
+        _id: '003'
+    },
+    {
+        title: 'Go to Costco',
+        details: 'Buy food',
+        _id: '004'
+    }
+]
