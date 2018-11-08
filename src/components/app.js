5daa736c90cf95705addc632a26fb82cd1085764
Andrew Ouatu <aouatu@outlook.com>
2018-11-08 15:22:13 -0800

    completed connect to server functionality

--- src/components/app.js
+++ src/components/app.js
@@ -2,10 +2,13 @@ import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize';
 import '../assets/css/app.css';
 import React, { Component } from 'react';
+import axios from 'axios';
 import List from './list';
 import AddItem from './add_item';
-import listData from "../dummy_data/list";
 import { randomString } from '../helpers';
+
+const BASE_URL = 'http://api.reactprototypes.com/todos';
+const API_KEY = '?key=c918_demoandrew';
 
 
 class App extends Component {
@@ -13,44 +16,86 @@ class App extends Component {
         super(props);
 
         this.state = {
-            list: []
+            list: [],
+            error: ''
         }
     }
 
-    deleteItem = (index) => {
-        const listCopy = this.state.list.slice();
+    deleteItem = async (id) => {
+        console.log('Delete item with ID:', id);
 
-        listCopy.splice(index, 1);
+        const response = await axios.delete(`${BASE_URL}/${id + API_KEY}`);
 
-        this.setState({
-            list: listCopy
-        });
+        console.log('Delete Response:', response);
+
+        this.getListData();
     };
 
-    addItem = (item) => {
-        item._id = randomString(8);
-        this.setState({
-            list: [item, ...this.state.list]
-        })
+    addItem = async (item) => {
+        const response = await axios.post(BASE_URL + API_KEY, item);
+
+        console.log('Add Item:', response);
+
+        this.getListData();
     };
 
     componentDidMount(){
         this.getListData();
     }
 
-    getListData(){
-        //Call server to get data
-        this.setState({
-            list: listData
-        })
-    }
+        //FOR ES7 AND UNDER (.THEN)
+        // getListData() {
+        //http://api.reactprototypes.com/todos?key=c718_demouser
+        // axios.get(BASE_URL + API_KEY).then((response) => {
+        //     console.log('Server Response:', response);
+        //
+        //     this.setState({
+        //         list: response.data.todos
+        //     })
+        // }).catch((error) => {
+        //     console.log('Request Error', error.message);
+        //     this.setState({
+        //         error: 'Error getting todos'
+        //     });
+        //  });
+        // }
+
+        //FOR ES7 AND ABOVE
+        //ASYNC-08 VERSION
+        async getListData() {
+            //http://api.reactprototypes.com/todos?key=c718_demouser
+            try {
+
+            const response = await axios.get(BASE_URL + API_KEY);
+
+            this.setState({
+                list: response.data.todos
+            });
+            } catch(error){
+                this.setState({
+                    error: 'Error getting todos'
+                });
+            }
+        }
+
+
+
+
     render() {
+        const { error } = this.state;
+
         return (
             <div className="container">
                 <h1 className="center">To Do List</h1>
 
                 <AddItem add={this.addItem}/>
-                <List delete={this.deleteItem} data={this.state.list}/>
+
+                {
+                    error
+                        ? <h1 className="center red-text">{error}</h1>
+                        : <List delete={this.deleteItem} data={this.state.list}/>
+                }
+
             </div>
         );
     }
